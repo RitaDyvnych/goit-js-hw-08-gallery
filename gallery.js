46fd1a8622775d1491f4cd001c94e2a76cd57a2d
RitaDyvnych <m.dyvnych@gmail.com>
2021-07-22 08:58:48 +0300

    открытие-закрытие модального окна, делегирование

--- gallery.js
+++ gallery.js
@@ -1,14 +1,14 @@
-import { galleryItems } from './app.js';
+import {galleryItems} from './app.js';
 
-const galleryArrayCollection = galleryItems.map(item => item.original);
+// Создание и рендер разметки по массиву данных galleryItems
 
 const galleryConteiner = document.querySelector('.js-gallery');
 const lightboxContainer = document.querySelector('.js-lightbox');
 const closeBtn = document.querySelector('.lightbox__button');
 const lightboxOverlay = document.querySelector('.lightbox__overlay');
 const lightboxImg = document.querySelector('.lightbox__image');
-
-
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+galleryConteiner.insertAdjacentHTML('beforeend', galleryMarkup);
 
 function createGalleryCardsMarkup (galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
@@ -29,4 +29,42 @@ function createGalleryCardsMarkup (galleryItems) {
         .join('');
 }
 
-console.log(createGalleryCardsMarkup(galleryItems));
+// Открытие модального окна по клику на элементе галереи. Закрытие модального окна
+// Подмена значения атрибута src, Очистка значения атрибута src
+
+galleryConteiner.addEventListener('click', onGalleryImageClick);
+const lightboxContainer = document.querySelector('.js-lightbox');
+const lightboxImg = document.querySelector('.lightbox__image');
+const closeBtn = document.querySelector('.lightbox__button');
+
+function onGalleryImageClick (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return
+  };
+
+  lightboxContainer.classList.add('is-open');
+  lightboxImg.setAttribute("src", e.target.dataset.source);
+  lightboxImg.setAttribute("alt", e.target.getAttribute("alt"));
+  lightboxContainer.addEventListener('click', onCloseBtnClick);
+  window.addEventListener("keydown", onKeyPress);
+}
+
+function onCloseBtnClick(e) {
+  if (e.target !== lightboxImg) {
+    lightboxContainer.classList.remove('is-open');
+    lightboxImg.src = "";
+    lightboxImg.alt = "";
+    closeBtn.removeEventListener('click', onCloseBtnClick);
+    window.removeEventListener("keydown", onKeyPress);
+  }
+};
+
+// Дополнительно Закрытие модального окна по нажатию клавиши ESC
+
+function onKeyPress(e) {
+  if (e.code === "Escape") {
+    onCloseBtnClick(e)
+  }
+}
+
